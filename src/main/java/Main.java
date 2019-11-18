281f366571c574264d6744cc6bb519930f21ae54
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2019-11-18 19:58:11 +0100

    Added option 2 to menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,38 @@
+import domain.Alcohol;
+import domain.Mixer;
+import service.ServiceData;
+
+import java.util.List;
 import java.util.Scanner;
 
-import static cli.Menu.showMenu;
+import static cli.Menu.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
+        ServiceData service = new ServiceData();
+
         int option;
 
-        showMenu();
-
         do {
+            showMenu();
             Scanner in = new Scanner(System.in);
             option = in.nextInt();
 
-        } while (option != 6);
+            switch (option) {
+                case 1:
+                    break;
+                case 2:
+                    Mixer cola = service.findMixerByName("Cola");
+                    List<Alcohol> alcohols = service.findByGraduationBetweenAndCombineWith(25 , 60, cola);
+                    System.out.println(alcohols);
+                    break;
+                default:
+                    System.out.println("Option not valid");
+            }
+
+        } while (option != MENU_EXIT);
 
     }
 }
